e8471162ca588b01ed0067b22d1606660452bae2
masterchan <contact@masterchan.me>
2015-04-01 19:37:08 -0400

    高大上的Apple Watch 扩展
    使用Xcode beta版本构建

---
+++ moegirlwiki/moegirlwiki WatchKit Extension/InterfaceController.h
@@ -0,0 +1,14 @@
+//
+//  InterfaceController.h
+//  moegirlwiki WatchKit Extension
+//
+//  Created by Michael Chan on 15/4/1.
+//  Copyright (c) 2015年 masterchan.me. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface InterfaceController : WKInterfaceController
+
+@end
---
+++ moegirlwiki/moegirlwiki WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  moegirlwiki WatchKit Extension
+//
+//  Created by Michael Chan on 15/4/1.
+//  Copyright (c) 2015年 masterchan.me. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
